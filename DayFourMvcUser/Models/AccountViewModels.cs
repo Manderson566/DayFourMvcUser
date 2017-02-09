c1f35894775752bb4fab517a06bb50d334ba9598
Michael Anderson <Micmail566@gmail.com>
2017-02-09 12:19:23 -0500

    todays notes need to update email to username

--- DayFourMvcUser/Models/AccountViewModels.cs
+++ DayFourMvcUser/Models/AccountViewModels.cs
@@ -49,9 +49,8 @@ namespace DayFourMvcUser.Models
     public class LoginViewModel
     {
         [Required]
-        [Display(Name = "Email")]
-        [EmailAddress]
-        public string Email { get; set; }
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
 
         [Required]
         [DataType(DataType.Password)]
@@ -65,12 +64,15 @@ namespace DayFourMvcUser.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "UserName")]
+        public string UserName { get; set; }
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
 
         [Required]
-        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 1)]
         [DataType(DataType.Password)]
         [Display(Name = "Password")]
         public string Password { get; set; }
